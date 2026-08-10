a845472adb7c528b81e729364f640fe4b9590075
Marceline <miss.knigomanka@gmail.com>
2026-08-10 22:14:25 +0400

    part2 [phonebook]: 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -47,29 +50,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={filter} onChange={handleFilterChange} />
-      </div>
+      <Filter value={filter} onChange={handleFilterChange} />
 
       <h2>add a new</h2>
-      <form onSubmit={handleAddNewPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNewNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNewNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        onSubmit={handleAddNewPerson}
+        name={{ value: newName, onChange: handleNewNameChange }}
+        number={{ value: newNumber, onChange: handleNewNumberChange }}
+      />
 
       <h2>Numbers</h2>
-      {getFilteredPersons().map((person) => (
-        <div key={person.id}>
-          {person.name} {person.number}
-        </div>
-      ))}
+      <Persons persons={getFilteredPersons()} />
     </div>
   );
 };
